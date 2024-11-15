2a94602ab5718f3220fc4b4deb039d12b51b6952
ALatronico <gabant47@gmail.com>
2024-11-15 09:59:15 +0100

    fix: less console log and only 1 string output

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-for (let i = 1; i <= 100; i++) {
-  if(!(i % 3) && !(i % 5)){
-    console.log('FizzBuzz');
-  } else if(!(i % 3)){
-    console.log('Fizz');
-  } else if (!(i % 5)){
-    console.log('Buzz');
-  } else{
-    console.log(i);
-  }
-}
+let message = '';
+
+for(let i = 1; i <=100; i++){
+    if(!(i % 15)){
+      message += 'FizzBuzz';
+    } else if(!(i % 3)){
+      message += 'Fizz';
+    } else if(!(i % 5)){
+      message += 'Buzz';
+    } else{
+      message += i;
+    }
+    if( i < 100){
+        message += ' - ';
+    }
+} 
+console.log(message);
